accc6d4206801bca773b141e2b3f7c89fae77700
caiochmn <caio.marques.novais@hotmail.com>
2023-05-11 20:48:28 -0300

    docker filer

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,12 @@
+final baseUrl = URI().main;
+
+class URI {
+  // SERVER
+  final String main = 'https://todolist-api.edsonmelo.com.br';
+
+  // LOCALHOST USING Android Virtual Device
+  final String devAVD = 'https://todolist-api.edsonmelo.com.br';
+
+  // LOCALHOST USING Browser
+  final String devBrowser = 'https://todolist-api.edsonmelo.com.br';
+}
